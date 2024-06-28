fc7e0d87088d98740afe0f3369ff4ec23cb05c0b
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-28 12:30:17 +0530

    Update pinterest.py

--- pinterest.py
+++ pinterest.py
@@ -75,9 +75,9 @@ API_HASH ="4c246fb0c649477cc2e79b6a178ddfaa"
 BOT_TOKEN ="7171428829:AAGIf6PxLuLTv-Pq4zdvu9rbF3zNX9qgfZg"
 TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 MONGO_DB ="mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
-LOG = os.environ.get("LOG", "-1002093247039")
-OWNER = os.environ.get("OWNER", "Farooq_is_KING")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "https://t.me/WD_Topic_Group")
+LOG =-1002093247039
+OWNER ="Farooq_is_KING"
+SUPPORT_CHAT ="https://t.me/WD_Topic_Group"
 LOG_GROUP_ID =-1002093247039
 
 bot = TelegramClient("pinterestbot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
