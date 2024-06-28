491dcc50748ce45dcd85d0b53e1a34e5aad8e693
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-28 12:26:17 +0530

    Update pinterest.py

--- pinterest.py
+++ pinterest.py
@@ -70,15 +70,15 @@ async def download_image(url):
     return TMP_DOWNLOAD_DIRECTORY + "pinterest_iamge.jpg"
 
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+API_ID =27383453
+API_HASH ="4c246fb0c649477cc2e79b6a178ddfaa"
+BOT_TOKEN ="7171428829:AAGIf6PxLuLTv-Pq4zdvu9rbF3zNX9qgfZg"
 TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
-MONGO_DB = os.environ.get("MONGO_DB", None)
-LOG = os.environ.get("LOG", "-1001961465245")
+MONGO_DB ="mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+LOG = os.environ.get("LOG", "-1002093247039")
 OWNER = os.environ.get("OWNER", "Farooq_is_KING")
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "https://t.me/WD_Topic_Group")
-LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", None))
+LOG_GROUP_ID =-1002093247039
 
 bot = TelegramClient("pinterestbot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
